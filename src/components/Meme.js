e572d5a3e258c72e79a66195dd8d4e554f651a12
XeronScript <ksasin3@gmail.com>
2022-07-08 22:04:13 +0200

    Base meme generator app

---
+++ src/components/Meme.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import memesData from "../memesData"
+import './Meme.css'
+
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url,
+        }))
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value,
+        }))
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top Text"
+                    className="form-input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom Text"
+                    className="form-input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button
+                    className="form-button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} alt="" className="meme-image"/>
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
+}
